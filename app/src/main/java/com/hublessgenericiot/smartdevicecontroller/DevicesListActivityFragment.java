0edb2482937151dab716a5952af99488706ef2a4
Ben Wencke <benwencke@gmail.com>
2016-01-30 14:12:21 -0500

    simple blank app

---
+++ app/src/main/java/com/hublessgenericiot/smartdevicecontroller/DevicesListActivityFragment.java
@@ -0,0 +1,22 @@
+package com.hublessgenericiot.smartdevicecontroller;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class DevicesListActivityFragment extends Fragment {
+
+    public DevicesListActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_devices_list, container, false);
+    }
+}

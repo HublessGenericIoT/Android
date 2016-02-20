57329e0f90d7bea63db7b66b7b30e14cd1f75aaa
Ben Wencke <benwencke@gmail.com>
2016-02-20 18:25:03 -0500

    automatically finds device

--- app/src/main/java/com/hublessgenericiot/smartdevicecontroller/RoomsActivity.java
+++ app/src/main/java/com/hublessgenericiot/smartdevicecontroller/RoomsActivity.java
@@ -1,9 +1,19 @@
 package com.hublessgenericiot.smartdevicecontroller;
 
+import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
+import android.os.Handler;
 import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -24,6 +34,8 @@ import android.widget.TextView;
 import com.hublessgenericiot.smartdevicecontroller.dummy.DummyContent;
 
 import java.util.LinkedList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class RoomsActivity extends AppCompatActivity implements DeviceFragment.OnListFragmentInteractionListener {
 
@@ -41,6 +53,11 @@ public class RoomsActivity extends AppCompatActivity implements DeviceFragment.O
      * The {@link ViewPager} that will host the section contents.
      */
     private ViewPager mViewPager;
+
+    WifiManager wifi;
+
+    String[] perms = {"android.permission.ACCESS_WIFI_STATE", "android.permission.CHANGE_WIFI_STATE", "android.permission.ACCESS_COARSE_LOCATION"};
+    private static final int MY_PERMISSIONS_REQUEST_WIFI = 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,10 +81,13 @@ public class RoomsActivity extends AppCompatActivity implements DeviceFragment.O
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                wifi.startScan();
+                Snackbar.make(view, "Performing scan...", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
+
+        scanForDevices();
 
     }
 
@@ -146,5 +166,117 @@ public class RoomsActivity extends AppCompatActivity implements DeviceFragment.O
         Intent intent = new Intent(this, EditDeviceActivity.class);
         intent.putExtra(EditDeviceActivity.DEVICE_ID, item.id);
         startActivityForResult(intent, EditDeviceActivity.DEVICE_EDITED);
+    }
+
+    private void scanForDevices() {
+
+//        // Here, thisActivity is the current activity
+//        if (ContextCompat.checkSelfPermission(this,
+//                Manifest.permission.ACCESS_WIFI_STATE)
+//                != PackageManager.PERMISSION_GRANTED) {
+//
+//            // Should we show an explanation?
+//            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+//                    Manifest.permission.ACCESS_WIFI_STATE)) {
+//
+//                // Show an expanation to the user *asynchronously* -- don't block
+//                // this thread waiting for the user's response! After the user
+//                // sees the explanation, try again to request the permission.
+//
+//                Log.d("TAG", "SHOW EXPLANATION!");
+//
+//            } else {
+//
+//                // No explanation needed, we can request the permission.
+//
+//                ActivityCompat.requestPermissions(this,
+//                        new String[]{Manifest.permission.READ_CONTACTS},
+//                        MY_PERMISSIONS_REQUEST_ACCESS_WIFI_STATE);
+//
+//                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+//                // app-defined int constant. The callback method gets the
+//                // result of the request.
+//
+//                Log.d("TAG", "Don't show explanation");
+//            }
+//        } else {
+//            Log.d("TAG", "granted");
+//            scanWifi();
+//        }
+
+        requestPermissions(perms, MY_PERMISSIONS_REQUEST_WIFI);
+    }
+
+    public void prepareScheduledScan() {
+        this.runOnUiThread(scheduledScan);
+    }
+
+    public Runnable scheduledScan = new Runnable() {
+        @Override
+        public void run() {
+            scanWifi();
+        }
+    };
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_WIFI: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    Log.d("TAG", "doing the thing");
+
+                    scanWifi();
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Log.d("TAG", "DENIED!");
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+    private void scanWifi() {
+
+        Log.d("dfg", "sc");
+
+        wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+
+        if (!wifi.isWifiEnabled()) {
+            wifi.setWifiEnabled(true);
+        }
+
+        registerReceiver(new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context c, Intent intent)  {
+                for(ScanResult s : wifi.getScanResults()) {
+                    Log.d("sc", s.SSID);
+                    if(s.SSID.startsWith("ESP_")) {
+                        Snackbar.make(findViewById(R.id.container), "New Device Found", Snackbar.LENGTH_LONG).show();
+                    }
+                }
+
+
+                new Timer().schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        prepareScheduledScan();
+                    }
+                }, 3000);
+            }
+        }, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+
+        wifi.startScan();
     }
 }
